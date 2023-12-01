e398ad7691bb2b8c3f128be2dd112ae72c160a93
Manuel Revilla <Manuel Revilla>
2023-12-01 18:56:20 +0100

    Más modificaciones, y comentarios

--- Entrega1/src/com/tewrrss/business/resteasy/LoginServiceRs.java
+++ Entrega1/src/com/tewrrss/business/resteasy/LoginServiceRs.java
@@ -2,12 +2,20 @@ package com.tewrrss.business.resteasy;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.tewrrss.dto.User;
 
+@Path("/LoginServiceRs")
 public interface LoginServiceRs {
+	
+	/* Método POST, para realizar un login de usuarios.
+	 * Recibe un JSON del lado del cliente, que aquí decodificará.
+	 * ¿Cómo se convierte el JSON al usuario? -> En el cliente y el servidor ha de tener las mismas.
+	 */
+	
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	@Produces({ MediaType.TEXT_PLAIN })
