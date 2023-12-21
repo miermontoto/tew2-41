0c61d10798890b91d46a3f3d7a1ac96224c86325
christian <uo276508@uniovi.es>
2023-12-21 16:08:02 +0100

    Servicios rest accesibles al exterior

--- Servidor/src/com/tewrrss/business/resteasy/Application.java
+++ Servidor/src/com/tewrrss/business/resteasy/Application.java
@@ -5,7 +5,9 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import impl.tewrrss.business.resteasy.CommunityServiceRsImpl;
 import impl.tewrrss.business.resteasy.LoginServiceRsImpl;
+import impl.tewrrss.business.resteasy.PostServiceRsImpl;
 import impl.tewrrss.business.resteasy.UserServiceRsImpl;
 
 @SuppressWarnings("unchecked")
@@ -14,9 +16,12 @@ public class Application extends javax.ws.rs.core.Application {
 	private Set<Class<?>> classes = new HashSet<Class<?>>();
 
 	public Application() {
-		//classes.add(AlumnosServicesRsImpl.class);
+
 		classes.add(LoginServiceRsImpl.class);
 		classes.add(UserServiceRsImpl.class);
+		classes.add(CommunityServiceRsImpl.class);
+		classes.add(PostServiceRsImpl.class);
+		
 	}
  	@Override
 	public Set<Class<?>> getClasses() {
