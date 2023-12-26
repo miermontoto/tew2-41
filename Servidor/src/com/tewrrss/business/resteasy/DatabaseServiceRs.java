c0d160a4c1ab095338f16b076bea96428d51e257
christian <uo276508@uniovi.es>
2023-12-26 14:33:26 +0100

    Arreglado errores en el reset y cambio del buildpath

--- Servidor/src/com/tewrrss/business/resteasy/DatabaseServiceRs.java
+++ Servidor/src/com/tewrrss/business/resteasy/DatabaseServiceRs.java
@@ -3,6 +3,7 @@ package com.tewrrss.business.resteasy;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.tewrrss.dto.PostToken;
@@ -12,6 +13,7 @@ public interface DatabaseServiceRs {
 
 	@POST
 	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Produces({ MediaType.TEXT_PLAIN })
 	boolean reset (String token);
 
 }
