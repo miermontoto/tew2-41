a5abf2781f26f9e5e56799748e792f5ecb0db247
Manuel Revilla <Manuel Revilla>
2024-01-12 18:42:06 +0100

    Arreglada carga JSON

--- Cliente/WebContent/javascript/functions_loadusers.js
+++ Cliente/WebContent/javascript/functions_loadusers.js
@@ -1,6 +1,6 @@
 document.getElementById("loadUsersButton").addEventListener("click", function() {
 	$.ajax({
-		url: "http://localhost:8080/Entrega1/redsocial.json", // Cambiar esta URL por la que corresponde de nuestro proyecto
+		url: "http://localhost:8080/Servidor/redsocial.json", // Cambiar esta URL por la que corresponde de nuestro proyecto
 		type: "GET",
 		dataType: "json",
 
@@ -15,7 +15,6 @@ document.getElementById("loadUsersButton").addEventListener("click", function() 
 					username: usuario.nombre
 				};
 			  	
-			  	document.getElementById("usuarioActual").innerHTML = "Cargando al usuario " + usuario.nombre;
 			  	contador = contador + 1; // Incremento el contador
 			  	
 				// Envío al servidor lo requerido
@@ -26,8 +25,24 @@ document.getElementById("loadUsersButton").addEventListener("click", function() 
 			});
 			
 			if(contador === listaUsuarios.length){
-				
+				 // Se ha cargado con éxito
+				showMessages("success"); // Llamo a la función que se encargará de mostrar que ha habido éxito, además de ocultar carga.
+			} else {
+				showMessages("error");
 			}
 		} // Cierre de la función de éxito (success)
 	}); // Cierre de $.ajax
 }); // Cierre del método addEventListener
+
+function showMessages(operation){
+
+	var opInfoSuccess = document.getElementById("opInfoSuccess"); // Muestro al usuario el anuncio
+	var opInfoFailure = document.getElementById("opInfoFailure");
+	if(operation === "success"){
+		opInfoSuccess.style.display = "block";
+		opInfoSuccess.innerHTML = "Operación realizada con éxito";
+	} else {
+		opInfoFailure.style.display = "block";
+		opInfoFailure.innerHTML = "Ha ocurrido un problema";
+	}
+}
