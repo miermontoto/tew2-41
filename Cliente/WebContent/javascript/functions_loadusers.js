c306384840590b6956abd8fa3ebc3bb67ab2c5cc
Manuel Revilla <Manuel Revilla>
2024-01-12 18:21:01 +0100

    Añadiendo caracteristicas a JSON

--- Cliente/WebContent/javascript/functions_loadusers.js
+++ Cliente/WebContent/javascript/functions_loadusers.js
@@ -1,12 +1,12 @@
 document.getElementById("loadUsersButton").addEventListener("click", function() {
 	$.ajax({
-		url: "http://localhost:8080/Servidor/redsocial.json", // Cambiar esta URL por la que corresponde de nuestro proyecto
+		url: "http://localhost:8080/Entrega1/redsocial.json", // Cambiar esta URL por la que corresponde de nuestro proyecto
 		type: "GET",
 		dataType: "json",
 
 		success: function(redsocial) {
 			let listaUsuarios = redsocial.usuarios;
-
+			let contador = 0; // Contador para mirar los que llevamos subidos
 			listaUsuarios.forEach(function(usuario) {
 			  	let user = {
 					email: usuario.email,
@@ -14,14 +14,20 @@ document.getElementById("loadUsersButton").addEventListener("click", function() 
 					role: 1,
 					username: usuario.nombre
 				};
-
-
+			  	
+			  	document.getElementById("usuarioActual").innerHTML = "Cargando al usuario " + usuario.nombre;
+			  	contador = contador + 1; // Incremento el contador
+			  	
 				// Envío al servidor lo requerido
 				UserServiceRs.add({
 					$entity: user,
 					$contentType: "application/json"
 				});
 			});
+			
+			if(contador === listaUsuarios.length){
+				
+			}
 		} // Cierre de la función de éxito (success)
 	}); // Cierre de $.ajax
 }); // Cierre del método addEventListener
